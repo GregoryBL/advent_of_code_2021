7418e10b96424065969d4c89ded9204399345fa5
Greg Berns-Leone <gberns.l@gmail.com>
2021-12-03 22:56:29 -0800

    Cleaned up

--- day4/lib/day4.ex
+++ day4/lib/day4.ex
@@ -38,116 +38,81 @@ defmodule Day4 do
     ]
   end
 
-  def check_line(num, line) do
+  def process_line(num, line) do
     Enum.filter(line, fn item -> item != num end)
   end
 
-  def check_board(num, board) do
-    Enum.map(board, fn l -> check_line(num, l) end)
-  end
-
-  def check_boards(inputs, boards) when length(inputs) == 0, do: boards
-
-  def check_boards(inputs, boards) do
-    [num | rest] = inputs
-    res = Enum.map(boards, fn b -> check_board(num, b) end)
-
-    case Enum.find(res, false, &check_board_for_win/1) do
-      false -> check_boards(rest, res)
-      win -> score_board(num, win)
-    end
-  end
-
-  def score_board(num, board) do
-    IO.inspect(board)
-    board_score = List.flatten(board) |> Enum.map(&String.to_integer/1) |> Enum.sum()
-    board_score * String.to_integer(num)
-  end
-
-  def check_board_for_win(board) do
-    flipped = Enum.zip(board) |> Enum.map(&Tuple.to_list/1)
-    Enum.any?(board, &Enum.empty?/1) || Enum.any?(flipped, &Enum.empty?/1)
-  end
-
-  def add_flipped_boards(boards) do
-    flipped =
-      Enum.map(boards, fn b ->
-        Enum.zip(b) |> Enum.map(&Tuple.to_list/1)
-      end)
-
-    boards ++ flipped
-  end
-
-  def check_board_connected(num, board_c) do
-    IO.inspect(board_c)
-
+  def process_board(num, board_c) do
     Enum.map(board_c, fn board ->
-      Enum.map(board, fn l -> check_line(num, l) end)
+      Enum.map(board, fn l -> process_line(num, l) end)
     end)
   end
 
-  def score_board_connected(num, board) do
-    IO.puts(num)
+  def score_board(num, board) do
     b = Enum.at(board, 0)
-    IO.inspect(b)
     board_score = List.flatten(b) |> Enum.map(&String.to_integer/1) |> Enum.sum()
-    IO.puts(board_score)
     board_score * String.to_integer(num)
   end
 
-  def check_board_for_win_connected(board_c) do
+  def check_board_for_win(board_c) do
     Enum.any?(board_c, fn board ->
       Enum.any?(board, &Enum.empty?/1)
     end)
   end
 
-  def check_boards_connected(inputs, boards) when length(inputs) == 0, do: boards
+  def add_flipped_boards(boards) do
+    Enum.map(boards, fn b ->
+      [Enum.zip(b) |> Enum.map(&Tuple.to_list/1), b]
+    end)
+  end
 
-  def check_boards_connected(inputs, boards) do
+  def find_best_board(inputs, boards) when length(inputs) == 0, do: boards
+
+  def find_best_board(inputs, boards) do
     [num | rest] = inputs
-    res = Enum.map(boards, fn b -> check_board_connected(num, b) end)
+    # Remove all examples of "num"
+    res = Enum.map(boards, fn b -> process_board(num, b) end)
 
-    case Enum.find(res, false, &check_board_for_win_connected/1) do
-      false -> check_boards_connected(rest, res)
-      win -> score_board_connected(num, win)
+    # If a board won, score it, else continue
+    case Enum.find(res, false, &check_board_for_win/1) do
+      false -> find_best_board(rest, res)
+      win -> score_board(num, win)
     end
   end
 
-  def add_flipped_boards_connected(boards) do
-    flipped =
-      Enum.map(boards, fn b ->
-        [Enum.zip(b) |> Enum.map(&Tuple.to_list/1), b]
-      end)
-  end
+  def find_worst_board(inputs, boards) when length(inputs) == 0, do: boards
 
-  def check_boards_lose(input, boards) do
+  def find_worst_board(input, boards) do
     [num | rest] = input
-    IO.inspect(boards)
-    IO.puts(num)
-    res = Enum.map(boards, fn b -> check_board_connected(num, b) end)
+    # Remove all examples of "num"
+    res = Enum.map(boards, fn b -> process_board(num, b) end)
 
-    remaining = Enum.filter(res, fn b -> not check_board_for_win_connected(b) end)
-    IO.inspect(remaining)
+    # Filter out won boards
+    remaining = Enum.filter(res, fn b -> not check_board_for_win(b) end)
 
+    # If we've none left, the board when we started was the winner. Score it
     case length(remaining) do
-      0 -> score_board_connected(num, Enum.at(res, 0))
-      _ -> check_boards_lose(rest, remaining)
+      0 -> score_board(num, Enum.at(res, 0))
+      _ -> find_worst_board(rest, remaining)
     end
   end
 
   def part2(input, boards) do
-    all_boards = add_flipped_boards_connected(boards)
-    check_boards_lose(input, all_boards)
+    # Boards in this step need to actually be connected. We store [ original, zipped ] for each board
+    all_boards = add_flipped_boards(boards)
+    find_worst_board(input, all_boards)
   end
 
   def part1(input, boards) do
-    # all_boards = add_flipped_boards_connected(boards)
-    check_boards(input, boards)
+    # This part didn't need them connected (just find the first completed), but was updated to use the same
+    # data structure as part 2
+    all_boards = add_flipped_boards(boards)
+    find_best_board(input, all_boards)
   end
 
   def main do
     [input | boards] = read_input_file("input-4.txt")
-    part1(input, boards)
-    part2(input, boards)
+    IO.puts(part1(input, boards))
+    IO.puts(part2(input, boards))
   end
 end
