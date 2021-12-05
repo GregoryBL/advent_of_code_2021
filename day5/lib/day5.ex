a4b677db4eaf886ccc74ba873ed5d13d91fb712e
Greg Berns-Leone <gberns.l@gmail.com>
2021-12-04 21:58:14 -0800

    A bit touched up

--- day5/lib/day5.ex
+++ day5/lib/day5.ex
@@ -1,37 +1,41 @@
 defmodule Day5 do
   def read_input_file(name) do
-    lines =
-      File.read!(name)
-      |> String.trim()
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn l ->
-        String.split(l, " -> ", trim: true)
-        |> Enum.map(fn p ->
-          String.split(p, ",")
-          |> Enum.map(&String.to_integer/1)
-        end)
+    # The parsing code is always gnarly but whatever
+    File.read!(name)
+    |> String.trim()
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn l ->
+      String.split(l, " -> ", trim: true)
+      |> Enum.map(fn p ->
+        String.split(p, ",")
+        |> Enum.map(&String.to_integer/1)
       end)
+    end)
   end
 
-  def is_horiz_vert?(line) do
-    [[x1, y1], [x2, y2]] = line
-
+  def is_horiz_vert?([[x1, y1], [x2, y2]]) do
     x1 == x2 || y1 == y2
   end
 
-  def make_points([[x1, y1], [x2, y2]]) when x1 == x2 do
-    0..(y2 - y1) |> Enum.map(fn n -> [x1, y1 + n] end)
-  end
+  # def make_points([[x1, y1], [x2, y2]]) when x1 == x2 do
+  #   0..(y2 - y1) |> Enum.map(fn n -> [x1, y1 + n] end)
+  # end
 
-  def make_points([[x1, y1], [x2, y2]]) when y1 == y2 do
-    0..(x2 - x1) |> Enum.map(fn n -> [x1 + n, y1] end)
-  end
+  # def make_points([[x1, y1], [x2, y2]]) when y1 == y2 do
+  #   0..(x2 - x1) |> Enum.map(fn n -> [x1 + n, y1] end)
+  # end
+
+  def direction(a, b) when a == b, do: 0
+  def direction(a, b) when a > b, do: 1
+  def direction(a, b) when a < b, do: -1
 
   def make_points([[x1, y1], [x2, y2]]) do
-    dx = if x2 > x1, do: 1, else: -1
-    dy = if y2 > y1, do: 1, else: -1
+    dx = direction(x2, x1)
+    dy = direction(y2, y1)
 
-    0..abs(x2 - x1) |> Enum.map(fn n -> [x1 + n * dx, y1 + n * dy] end)
+    num_points = max(abs(x2 - x1), abs(y2 - y1))
+
+    0..num_points |> Enum.map(fn n -> [x1 + n * dx, y1 + n * dy] end)
   end
 
   def part1(lines) do
